ad044edc13dfb92df94266bf9da347d334d1979f
Ole <olejohska@gmail.com>
2021-01-06 21:39:39 +0100

    Implemented CSS-only header invert styles.

--- assets/js/getFooter.js
+++ assets/js/getFooter.js
@@ -1,6 +1,7 @@
 (function getFooter() {
   document.getElementById("footer").innerHTML = `<div class="footernav">
   <ul class="workfooter">
+    <li class="footer"><a href="houstonchronicle.html" class="underlined animated">Houston Chronicle</a></li>
     <li class="footer"><a href="unfold.html" class="underlined animated">Unfold</a></li>
     <li class="footer"><a href="inydsr.html" class="underlined animated">Is The New York Dream Still Real?</li>
     <li class="footer"><a href="goods.html" class="underlined animated">Goods</a></li>
---
+++ assets/js/slickFunctions.js
@@ -0,0 +1,13 @@
+$('.slides').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  fade: false,
+  dots: true,
+  nextArrow: "<div class='slick-next'><div class='cursor-arrow'></div></div>",
+  prevArrow: "<div class='slick-prev'><div class='cursor-arrow'></div></div>",
+});
+
+$(".slick-arrow").mousemove(function(e) {
+  $(this).find(".cursor-arrow").css("top",e.clientY+"px").css("left",e.clientX+"px");
+})
