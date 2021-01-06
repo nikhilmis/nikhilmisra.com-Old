2ca3fcc1f9ed289addf981812401def2e727abc6
Ole <olejohska@gmail.com>
2021-01-06 17:53:57 +0100

    Moved generation of footer to JS. Minor fixes.

---
+++ assets/js/getFooter.js
@@ -0,0 +1,11 @@
+(function getFooter() {
+  document.getElementById("footer").innerHTML = `<div class="footernav">
+  <ul class="workfooter">
+    <li class="footer"><a href="unfold.html" class="underlined animated">Unfold</a></li>
+    <li class="footer"><a href="inydsr.html" class="underlined animated">Is The New York Dream Still Real?</li>
+    <li class="footer"><a href="goods.html" class="underlined animated">Goods</a></li>
+    <li class="footer"><a href="perspective.html" class="underlined animated">Perspective</a></li>
+    <li class="footer"><a href="posters.html" class="underlined animated">Posters</a></li>
+  </ul>
+</div>`;
+})()
