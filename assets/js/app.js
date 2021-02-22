fdb9f852038a687aee98634a3dcae27899201791
Ole Skaar <olejohska@gmail.com>
2021-02-22 23:00:49 +0100

    Changed to insert p elements if multiple line breaks
    * Also fixed first element disappearing when selecting multiple

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,11 +22,12 @@ document.addEventListener('selectionchange',  function() {
       text = window.getSelection().toString();
       parentElement = getSelectableParent(window.getSelection().anchorNode);
 
-  } else if (document.selection && document.selection.type != "Control") {
-      selection = document.selection
-      text = document.selection.createRange().text;
-      parentElement = window.getSelection().anchorNode.parentElement;
-  }
+  } 
+  // else if (document.selection && document.selection.type != "Control") {
+  //     selection = document.selection
+  //     text = document.selection.createRange().text;
+  //     parentElement = window.getSelection().anchorNode.parentElement;
+  // }
   
   if (text.length > 0) {
       // Hide original parent element
@@ -42,9 +43,20 @@ document.addEventListener('selectionchange',  function() {
       
       // Insert copy of in relative position matching boundingClientRect of original parent
       const { x, y, width } = parentElement?.getBoundingClientRect();
+      const paragraphs = text.match(/[^\r\n]+/g);
+      const indentedHeaderParagraphs = $(parent).find('.header-expanded-bio').length !== 0;
+      
+      let textElement;
+      if (paragraphs.length > 1) {
+        textElement = paragraphs.map((p, index) => `
+        <p class="highlighted highlighted-paragraph ${index > 0 && indentedHeaderParagraphs ? ' header-expanded-bio' : ''}">${p}</p>
+      `).join('')
+      } else {
+        textElement = `<span class="highlighted">${text}</span>`;
+      }
       var elementWithBlur = `
           <div id="highlighted-text" style="position: absolute; top: ${y}px; left: ${x}px; width: ${width}px;">
-            <span class="blur">${textBeforeSelection}</span><span class="highlighted">${text}</span><span class="blur">${textAfterSelection}</span>
+            <span class="blur">${textBeforeSelection}</span>${textElement}<span class="blur">${textAfterSelection}</span>
           </div>
         `
       
@@ -54,6 +66,7 @@ document.addEventListener('selectionchange',  function() {
 
 function getSelectableParent(element) {
   switch (element.nodeName) {
+    case "HEADER":
     case "BODY":
     case "HTML":
       return undefined;
@@ -63,6 +76,7 @@ function getSelectableParent(element) {
     case "H3":
     case "H4":
     case "H5":
+    case "SECTION":
       return element;
     default:
       return getSelectableParent(element.parentElement)
@@ -76,14 +90,17 @@ function getTextBeforeSelection(element, prevText, selection) {
   if (element === anchorNode || element.firstChild === anchorNode) {
     text += elementText.substring(0, anchorOffset);
     return text;
-  } else {
+  } else if (element.nextSibling) {
     text += elementText;
     return getTextBeforeSelection(element.nextSibling, text, selection)
   }
 }
 
 function getTextAfterSelection(element, prevText, selection, childNodes) {
-  const elementIsInArray = Array.from(childNodes).indexOf(element) > -1;
+  const parentChildNodes = Array.from(childNodes);
+  const elementIsChild = parentChildNodes.indexOf(element) > -1;
+  const elementParentIsChild = parentChildNodes.indexOf(element.parentElement) > -1;
+  
   let text = prevText;
   const { focusNode, focusOffset } = selection;
   const elementText = element.innerText ?? element.wholeText;
@@ -94,9 +111,9 @@ function getTextAfterSelection(element, prevText, selection, childNodes) {
     text += elementText;
   }
 
-  if (element.nextSibling && elementIsInArray) {
+  if (element.nextSibling && elementIsChild) {
     return getTextAfterSelection(element.nextSibling, text, selection, childNodes);
-  } else if (element.parentElement.nextSibling && elementIsInArray) {
+  } else if (element.parentElement.nextSibling && (elementIsChild || elementParentIsChild)) {
     return getTextAfterSelection(element.parentElement.nextSibling, text, selection, childNodes) 
   } else {
     return text;
