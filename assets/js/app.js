58c807cad2734ad3998a36ceb6aad2a7a5121a40
Ole Skaar <olejohska@gmail.com>
2021-03-04 17:50:54 +0100

    Changed to a (hopefully) more robust implementation that relies on Range to a larger degree

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,7 +24,7 @@ document.addEventListener('selectionchange', function () {
     $(".blur-wrapper").removeClass("blur");
     $(".hideHighlightedElement").removeClass("hideHighlightedElement");
     $("#highlighted-text").remove();
-
+    
     let text = "";
     let selection = window.getSelection();
     if (selection) {
@@ -35,80 +35,120 @@ document.addEventListener('selectionchange', function () {
      }
 
     if (text.length > 0) {
+        if (selection.rangeCount > 1) {
+            combineRanges(selection);
+        }
         const range = selection.getRangeAt(0);
         const { commonAncestorContainer, startContainer, startOffset, endContainer, endOffset } = range;
         const { nodeName } = commonAncestorContainer
         if (!(nodeName === 'HEADER' || nodeName === 'BODY' || nodeName === 'HTML')) {
 
+            // Get common ancestor of elements in range
             const parentElement = getSelectableParent(commonAncestorContainer);
-            const nodeIterator = document.createNodeIterator(parentElement);
+           
+            //const nodeIterator = document.createNodeIterator(parentElement);
+            // while (nodeIterator.nextNode()) {
+            //     const { referenceNode: node } = nodeIterator;
+            //     if (nodes.length === 0 && node !== startContainer) continue;
+            //     if (!(node.nodeName === "#text" && node.textContent.includes('\n'))) {
+            //         nodes.add(getSelectableParent(node));
+            //     } 
+            //     if (node === endContainer) break;
+            // }
+            
+            // Add all direct child nodes of parent to array
+            const nodeArray = Array.from(parentElement.childNodes).filter(node => !(node.nodeName === "#text" && node.textContent.includes('\n')));
 
-            var nodes = new Set();
+            
+            // ALTERNATIVE: 
+            // Directly manipulate nodes in range
 
-            // Add all parent elements to array
+            // Potentially: https://developer.mozilla.org/en-US/docs/Web/API/Node/normalize
 
+            // Make a copy of nodes
+            
+
+            // Insert copy of in relative position matching boundingClientRect of original parent
+            // let parentElement;
+            // if (nodeArray.length === 0) {
+            //     parentElement = document.querySelector('body');
+            // } else if (nodeArray.length === 1) {
+            //     parentElement = nodeArray[0];
+            // } else if (nodeArray.length > 1) {
+            //     parentElement = nodeArray[0].parentElement;
+            // }
+            const { x, y, width } = parentElement.getBoundingClientRect();
+            const elementWithBlur = document.createElement('div');
+            elementWithBlur.classList = parentElement.classList;
+            $(elementWithBlur).attr('id', 'highlighted-text').css({ 'position': 'absolute', 'top': `${y}px`, 'left': `${x}px`, 'width': `${width}px` })
+            
+            // Hide original nodes
+            
+
+            // Add clone nodes to elementWithBlur
+            const cloneNodes = nodeArray.map((node) => node.cloneNode(true));
+            cloneNodes.forEach(node => $(elementWithBlur).append(node));
+
+            const cloneRange = document.createRange();
+
+            const nodeIterator = document.createNodeIterator(elementWithBlur);
             while (nodeIterator.nextNode()) {
                 const { referenceNode: node } = nodeIterator;
-                if (nodes.length === 0 && node !== startContainer) continue;
-                if (!(node.nodeName === "#text" && node.textContent.includes('\n'))) {
-                    if (node.nodeName !== "DIV") {
-                        nodes.add(getSelectableParent(node));
-                    } 
-                } 
+                if (node.data === startContainer.data) {
+                    cloneRange.setStart(node, startOffset);
+                }
+                if (node.data === endContainer.data) {
+                    cloneRange.setEnd(node, endOffset)
+                }
                 if (node === endContainer) break;
             }
 
-            const nodeArray = Array.from(nodes);
+            nodeArray.forEach(node => $(node).addClass('hideHighlightedElement'));
+            parentElement.classList.add('hideHighlightedElement');
 
-            // Make a copy of and hide originals
-            const cloneNodes = nodeArray.map((node) => node.cloneNode(true));
-            nodes.forEach(node => $(node).addClass('hideHighlightedElement'));
+            console.log(cloneRange)
+            // Iterate through all clone nodes and descendants and set blur and highlight tags where appropriate
+            // If it doesn't intersect range, it should get the blur class
+            // If it does intersect range, and if it doesn't contain start or end container, it should get the highlight class
+            // If it intersects range and contains start or end, add blur and highlight tags where appropriate
 
-            // Insert copy of in relative position matching boundingClientRect of original parent
-            let outermostElement;
-            if (nodeArray.length === 0) {
-                outermostElement = document.querySelector('body');
-            } else if (nodeArray.length === 1) {
-                outermostElement = nodeArray[0];
-            } else if (nodeArray.length > 1) {
-                outermostElement = nodeArray[0].parentElement;
-            }
-            const { x, y, width } = outermostElement.getBoundingClientRect();
-            const elementWithBlur = document.createElement('div');
-            $(elementWithBlur).attr('id', 'highlighted-text').css({ 'position': 'absolute', 'top': `${y}px`, 'left': `${x}px`, 'width': `${width}px` })
+            // cloneNodes.forEach((node) => {
+            //     const childNodes = getTextNodes(node);
+            //     const containsStartContainer = childNodes.find(childNode => childNode.data === startContainer.data);
+            //     const containsEndContainer = childNodes.find(childNode => childNode.data === endContainer.data);
+            //     const actualStartOffset = !containsStartContainer ? 0 : getActualOffset(childNodes, childNodes.indexOf(containsStartContainer), startOffset);
+            //     const actualEndOffset = !containsEndContainer ? 0 : getActualOffset(childNodes, childNodes.indexOf(containsEndContainer), endOffset);
 
-            cloneNodes.forEach((node) => {
-                const childNodes = getTextNodes(node);
-                const containsStartContainer = childNodes.find(childNode => childNode.data === startContainer.data);
-                const containsEndContainer = childNodes.find(childNode => childNode.data === endContainer.data);
-                const actualStartOffset = !containsStartContainer ? 0 : getActualOffset(childNodes, childNodes.indexOf(containsStartContainer), startOffset);
-                const actualEndOffset = !containsEndContainer ? 0 : getActualOffset(childNodes, childNodes.indexOf(containsEndContainer), endOffset);
-
-                const p = document.createElement('p');
-                p.classList = node.classList;
-                p.classList.add('cloneParagraph');
+            //     const p = document.createElement(node.nodeName.toLowerCase());
+            //     p.classList = node.classList;
+            //     p.classList.add('cloneParagraph');
                 
-                if (!containsStartContainer && !containsEndContainer) {
-                    p.innerHTML = `<span class="${$(elementWithBlur).has('.highlighted').length > 0 ? "highlighted" : "blur"}">${node.innerText}</span>`;
-                } else if (containsStartContainer && !containsEndContainer) {
-                    const highlightFromStart = startOffset === 0;
-                    p.innerHTML = `${highlightFromStart ? '' : '<span class="blur">'}${node.innerText.substring(0, actualStartOffset)}${highlightFromStart ? '' : '</span>'}<span class="highlighted">${node.innerText.substring(actualStartOffset)}</span>`;
-                } else if (!containsStartContainer && containsEndContainer) {
-                    p.innerHTML = `<span class="highlighted">${node.innerText.substring(0, actualEndOffset)}</span><span class="blur">${node.innerText.substring(actualEndOffset)}</span>`;
-                } else if (containsStartContainer && containsEndContainer) {
-                    const highlightFromStart = startOffset === 0;
-                    const highlightEndsAtEnd = endOffset === node.innerText.length -1;
-                    p.innerHTML = `${highlightFromStart ? '' : '<span class="blur">'}${node.innerText.substring(0, actualStartOffset)}${highlightFromStart ? '' : '</span>'}<span class="highlighted">${node.innerText.substring(actualStartOffset, actualEndOffset)}</span>${highlightEndsAtEnd ? '' : '<span class="blur">'}${node.innerText.substring(actualEndOffset)}${highlightEndsAtEnd ? '' : '</span>'}`
-                }
+            //     if (!containsStartContainer && !containsEndContainer) {
+            //         p.innerHTML = `<span class="${$(elementWithBlur).has('.highlighted').length > 0 ? "highlighted" : "blur"}">${node.innerText}</span>`;
+            //     } else if (containsStartContainer && !containsEndContainer) {
+            //         const highlightFromStart = startOffset === 0;
+            //         p.innerHTML = `${highlightFromStart ? '' : '<span class="blur">'}${node.innerText.substring(0, actualStartOffset)}${highlightFromStart ? '' : '</span>'}<span class="highlighted">${node.innerText.substring(actualStartOffset)}</span>`;
+            //     } else if (!containsStartContainer && containsEndContainer) {
+            //         p.innerHTML = `<span class="highlighted">${node.innerText.substring(0, actualEndOffset)}</span><span class="blur">${node.innerText.substring(actualEndOffset)}</span>`;
+            //     } else if (containsStartContainer && containsEndContainer) {
+            //         const highlightFromStart = startOffset === 0;
+            //         const highlightEndsAtEnd = endOffset === node.innerText.length -1;
+            //         p.innerHTML = `${highlightFromStart ? '' : '<span class="blur">'}${node.innerText.substring(0, actualStartOffset)}${highlightFromStart ? '' : '</span>'}<span class="highlighted">${node.innerText.substring(actualStartOffset, actualEndOffset)}</span>${highlightEndsAtEnd ? '' : '<span class="blur">'}${node.innerText.substring(actualEndOffset)}${highlightEndsAtEnd ? '' : '</span>'}`
+            //     }
 
-                
-                $(elementWithBlur).append(p)
+            //     $(elementWithBlur).append(p)
 
+            // });
+            //console.log(range)
+            cloneNodes.forEach(node => {
+                //console.log(range.intersectsNode(node))
             });
 
+            
             if ($(elementWithBlur).children().length > 0) {
                 // Blur everything
                 $(".blur-wrapper").addClass("blur");
+                
             }
 
             $('body').prepend(elementWithBlur);
@@ -156,4 +196,23 @@ function getTextNodes(node) {
         textNodes.push(currentTextNode)
     }
     return textNodes;
+}
+
+// function blurAllNonSelectionNodes(selection) {
+//     const allNodes = Array.from(document.getElementsByTagName('*'));
+//     console.log(selection)
+//     allNodes.forEach(node => {
+//         if (node.nodeName !== "HTML" && node.nodeName !== "BODY" && node.nodeName !== "HEADER" && node.nodeName !== "DIV") {
+//             console.log(selection.containsNode(node))
+//             if (!selection.containsNode(node)) {
+//                 node.classList.add('blur');
+//             }
+//         }
+//     })
+// }
+
+function combineRanges(selection) {
+    const mainRange = selection.getRangeAt(0);
+    const lastRange = selection.getRangeAt(selection.rangeCount - 1);
+    mainRange.setEnd(lastRange.endContainer, lastRange.endOffset);
 }
